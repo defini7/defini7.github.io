5645eb3520fd6aaa1b308f06857bb0e3d625847b
Alex <75216201+defini7@users.noreply.github.com>
2023-09-11 21:12:24 +0300

    Delete imgview directory

--- imgview/src/index.js
+++
@@ -1,9 +0,0 @@
-function grabData(input) {
-    let URL = window.URL || window.webkitURL;
-    const file = input.files[0];
-
-    if (file) {
-        const img = document.querySelector("img");
-        img.src = URL.createObjectURL(file);
-    }
-}
